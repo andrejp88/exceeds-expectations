c777c5d84f1aa39534bf71f3c60982c037b628c2
andrej88 <1616076-andrej88@users.noreply.gitlab.com>
2020-07-19 20:43:39 +0100

    Name to_approximately_equal tests

--- source/exceeds_expections/test/to_approximately_equal.d
+++ source/exceeds_expections/test/to_approximately_equal.d
@@ -3,20 +3,19 @@ module exceeds_expections.test.to_approximately_equal;
 import exceeds_expections;
 import exceeds_expections.test;
 
+@("float approximatelyEqualTo float")
 unittest
 {
-    expect(2.9999999999).toApproximatelyEqual(3.0);
-    expect(2.9999999999).toBeCloseTo(3.0);
-
+    expect(float(2.9999999999)).toApproximatelyEqual(float(3.0));
+    expect(float(2.9999999999)).toBeCloseTo(float(3.0));
 
     showMessage(
         expect(2.9999999999).toEqual(3.0)
     );
 }
 
+@("float !toApproximatelyEqual float")
 unittest
 {
-    showMessage(
-        expect(2.9).toApproximatelyEqual(3.0)
-    );
+    showMessage(expect(2.9).toApproximatelyEqual(3.0));
 }
