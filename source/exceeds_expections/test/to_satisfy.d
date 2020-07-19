33d72fdb2019978a44d4202d2e59aec9a7c0b779
andrej88 <1616076-andrej88@users.noreply.gitlab.com>
2020-07-19 21:54:34 +0100

    Name and clean up toSatisfy tests

--- source/exceeds_expections/test/to_satisfy.d
+++ source/exceeds_expections/test/to_satisfy.d
@@ -3,11 +3,13 @@ module exceeds_expections.test.to_satisfy;
 import exceeds_expections;
 import exceeds_expections.test;
 
+@("Integer satisfy 1/1")
 unittest
 {
     showMessage(expect(45).toSatisfy(a => a < 32 && a > 23));
 }
 
+@("Class satisfy 1/1")
 unittest
 {
     import std.math : sqrt;
@@ -29,16 +31,19 @@ unittest
     expect(new A(2, 3, 6)).toSatisfy(a => sqrt(a.x * a.x + a.y * a.y + a.z * a.z) == 7);
 }
 
+@("Integer satisfyAll 3/3")
 unittest
 {
-    // No failures
     expect(5).toSatisfyAll(
         e => e < 10,
         e => e > 2,
         e => e % 2 == 1
     );
+}
 
-    // One failure
+@("Integer satisfyAll 2/3")
+unittest
+{
     showMessage(
         expect(5).toSatisfyAll(
             e => e < 10,
@@ -46,8 +51,11 @@ unittest
             e => e % 2 == 1
         )
     );
+}
 
-    // Two failures
+@("Integer satisfyAll 2/3")
+unittest
+{
     showMessage(
         expect(5).toSatisfyAll(
             e => e < 3,
@@ -56,7 +64,11 @@ unittest
         )
     );
 
-    // Three failures
+}
+
+@("Integer satisfyAll 3/4")
+unittest
+{
     showMessage(
         expect(5).toSatisfyAll(
             e => e < 3,
@@ -77,6 +89,13 @@ unittest
     );
 }
 
+@("Integer satisfyAny 3/3")
+unittest
+{
+    expect(0).toSatisfyAll(i => i < 100, i => i > -1, i => i == 0);
+}
+
+@("Integer satisfyAny 2/3")
 unittest
 {
     // Passes
@@ -85,7 +104,11 @@ unittest
         e => e > 9,
         e => e % 2 == 1
     );
+}
 
+@("Integer satisfyAny 0/3")
+unittest
+{
     // Fails
     showMessage(
         expect(5).toSatisfyAny(
@@ -96,9 +119,8 @@ unittest
     );
 }
 
+@("char satifsfyAny 1/2")
 unittest
 {
-    expect(5).toSatisfy(e => e == 5);
     expect('c').toSatisfyAny(c => c == 'c', c => c == 'd');
-    expect(0).toSatisfyAll(i => i < 100, i => i > -1, i => i == 0);
 }
