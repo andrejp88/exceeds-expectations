3d59b76801d555cc43a4a39c9ccbb14b1db86756
andrej88 <1616076-andrej88@users.noreply.gitlab.com>
2020-07-22 22:27:32 +0100

    Re-arrange not-tests

--- source/exceeds_expections/test/not.d
+++ source/exceeds_expections/test/not.d
@@ -3,66 +3,83 @@ module exceeds_expections.test.not;
 import exceeds_expections;
 import exceeds_expections.test;
 
+@("toEqual")
 unittest
 {
     expect(4).not.toEqual(5);
 }
 
-unittest
-{
-    expect(4).not.toBe(5);
-}
-
-unittest
-{
-    expect(4).not.toApproximatelyEqual(5.0);
-}
-
-unittest
-{
-    expect(4).not.toSatisfy(i => i == 5);
-}
-
-unittest
-{
-    expect(4).not.toSatisfyAny(i => i == 5, i => i == 3);
-}
-
-unittest
-{
-    expect(4).not.toSatisfyAll(i => i == 4, i => i == 5);
-}
-
+@("toEqual, but does")
 unittest
 {
     shouldFail(expect(4).not.toEqual(4));
 }
 
+@("toBe")
+unittest
+{
+    expect(4).not.toBe(5);
+}
+
+@("toBe, but does")
 unittest
 {
     shouldFail(expect(4).not.toBe(4));
 }
 
+@("toApproximatelyEqual")
+unittest
+{
+    expect(4).not.toApproximatelyEqual(5.0);
+}
+
+@("toApproximatelyEqual, but does")
 unittest
 {
     shouldFail(expect(4).not.toApproximatelyEqual(4.0));
 }
 
+
+@("toSatisfy")
+unittest
+{
+    expect(4).not.toSatisfy(i => i == 5);
+}
+
+@("toSatisfy, but does")
 unittest
 {
     shouldFail(expect(4).not.toSatisfy(i => i == 4));
 }
 
+
+@("toSatisfyAny")
+unittest
+{
+    expect(4).not.toSatisfyAny(i => i == 5, i => i == 3);
+}
+
+@("toSatisfyAny, but does")
 unittest
 {
     shouldFail(expect(4).not.toSatisfyAny(i => i == 5, i => i == 4));
 }
 
+
+
+@("toSatisfyAll")
+unittest
+{
+    expect(4).not.toSatisfyAll(i => i == 4, i => i == 5);
+}
+
+@("toSatisfyAll, but does")
 unittest
 {
     shouldFail(expect(4).not.toSatisfyAll(i => i == 4, i => i <= 5));
 }
 
+@("Double negative should not be allowed")
 unittest
 {
     shouldFail(expect(4).not.not);
