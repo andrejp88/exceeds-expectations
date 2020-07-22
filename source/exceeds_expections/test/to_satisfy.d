ecf5ba9e8ab0a0c8c59fecbcb540cfe48463d1db
andrej88 <1616076-andrej88@users.noreply.gitlab.com>
2020-07-22 21:37:04 +0100

    Add tests for toSatisfy

--- source/exceeds_expections/test/to_satisfy.d
+++ source/exceeds_expections/test/to_satisfy.d
@@ -3,30 +3,43 @@ module exceeds_expections.test.to_satisfy;
 import exceeds_expections;
 import exceeds_expections.test;
 
+
+@("Integer success")
+unittest
+{
+    expect(45).toSatisfy(a => a % 2 == 1);
+}
+
 @("Integer failure")
 unittest
 {
     shouldFail(expect(45).toSatisfy(a => a < 32 && a > 23));
 }
 
+private class A
+{
+    real x;
+    real y;
+    real z;
+
+    this (real x, real y, real z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
+
 @("Class success")
 unittest
 {
     import std.math : sqrt;
-
-    class A
-    {
-        real x;
-        real y;
-        real z;
-
-        this (real x, real y, real z)
-        {
-            this.x = x;
-            this.y = y;
-            this.z = z;
-        }
-    }
-
     expect(new A(2, 3, 6)).toSatisfy(a => sqrt(a.x * a.x + a.y * a.y + a.z * a.z) == 7);
+}
+
+@("Class failure")
+unittest
+{
+    import std.math : sqrt;
+    shouldFail(expect(new A(4, 5, 7)).toSatisfy(a => sqrt(a.x * a.x + a.y * a.y + a.z * a.z) == 9));
 }
