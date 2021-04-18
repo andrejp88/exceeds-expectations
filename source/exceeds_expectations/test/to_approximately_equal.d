af3dd63854a4237e38bebd24ca0bd0ed8c4c5008
andrej88 <1616076-andrej88@users.noreply.gitlab.com>
2021-04-18 18:56:18 +0100

    Improve toApproximatelyEqual tests

--- source/exceeds_expectations/test/to_approximately_equal.d
+++ source/exceeds_expectations/test/to_approximately_equal.d
@@ -3,20 +3,22 @@ module exceeds_expectations.test.to_approximately_equal;
 import exceeds_expectations;
 import exceeds_expectations.test;
 
-@("float toApproximatelyEqual float")
+@("double toApproximatelyEqual double")
 unittest
 {
-    expect(float(2.9999999999)).toApproximatelyEqual(float(3.0));
+    expect(double(2.9999999999)).toApproximatelyEqual(double(3.0));
 
     shouldFail(
-        expect(2.9999999999).toEqual(3.0)
+        expect(double(2.9999999999)).toEqual(double(3.0))
     );
 }
 
 @("float !toApproximatelyEqual float")
 unittest
 {
-    shouldFail(expect(2.9).toApproximatelyEqual(3.0));
+    shouldFail(
+        expect(float(2.9)).toApproximatelyEqual(float(3.0))
+    );
 }
 
 @("float toApproximatelyEqual int")
